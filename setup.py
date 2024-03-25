d451b18451ab6d24cc9fad705f0851e23414230a
buyfakett <buyfakett@vip.qq.com>
2024-03-25 21:18:15 +0800

    v1.0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with io.open("requirements.txt", 'r') as f:
 
 setup(
     name='pyresp',
-    version='1.0.1',
+    version='1.0.2',
     description='fastapi的通用返回助手',
     author='buyfakett',
     author_email='buyfakett@vip.qq.com',
