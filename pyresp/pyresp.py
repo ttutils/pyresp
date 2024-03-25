1219e8c5429ddaf8153d0d6910e038d01b24285f
buyfakett <buyfakett@vip.qq.com>
2024-03-25 21:17:39 +0800

    fix: 增加打印日志

--- pyresp/pyresp.py
+++ pyresp/pyresp.py
@@ -13,20 +13,20 @@ def resp_200(code: int = 0, data: Union[List[dict], list, dict, str, float] = No
     Union：代表传入的参数可以是多种类型
     """
     if data is None:
-        return JSONResponse(
-            status_code=status.HTTP_200_OK,
-            content={
+        response_data = {
                 'code': code,
                 'message': message,
             }
-        )
-    return JSONResponse(
-        status_code=status.HTTP_200_OK,
-        content={
+    else:
+        response_data = {
             'code': code,
             'message': message,
             'data': data,
         }
+    logging.info(f'接口返回：{response_data}')
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content=response_data
     )
 
 
@@ -35,11 +35,12 @@ def resp_400(code: int = 400, message='错误，请重试！'):
     400系列的响应结构体
     *：代表调用方法时必须传参数
     """
-    logging.error(message)
+    response_data = {
+        'code': code,
+        'message': message,
+    }
+    logging.error(f'接口返回：{response_data}')
     return JSONResponse(
         status_code=status.HTTP_400_BAD_REQUEST,
-        content={
-            'code': code,
-            'message': message,
-        }
+        content=response_data
     )
